19abaa79b269137847777c0e460d09bc24d800b5
Joshua <joshua.yehudaioff@gmail.com>
2024-01-29 03:25:45 -0500

    Stuff

--- Assets/Scenes/TutorialExclusive/TutorialEvents.cs
+++ Assets/Scenes/TutorialExclusive/TutorialEvents.cs
@@ -101,6 +101,7 @@ public class TutorialEvents : EventParent
         onUpdate += countPatience;
         onUpdate += TurnTutCheckClear;
         LectureChange(turnLecture);
+
     }
 
     /// <summary>
